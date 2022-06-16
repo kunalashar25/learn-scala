0550e3b522926fb58e5fb5dfa319e0f8ec7eb9a0
Kunal Ashar <kunalashar.25@gmail.com>
2022-06-16 15:51:28 +0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "learn-scala"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
---
+++ src/main/scala/Object/HelloWorld.scala
@@ -0,0 +1,8 @@
+package Object
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+
+}
