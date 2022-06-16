b86593efa1cb971d73abf9e4e8f0fd06188c990c
Kunal Ashar <kunalashar.25@gmail.com>
2022-06-16 16:02:16 +0800

    ReadMe added

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learn Scala
+
+This repo will cover all the concepts required to learn Scala from the scratch
+
+I have created this repo as a part of self-learning and development activity by referring the Youtube Playlist - [Scala Tutorial](https://www.youtube.com/playlist?list=PLS1QulWo1RIagob5D6kMIAvu7DQC5VTh3)
+
+## Learning Flow
+1. [Created Singleton HelloWorld Object with `main()`](https://github.com/kunalashar25/learn-scala/blob/main/src/main/scala/Object/HelloWorld.scala)
--- src/main/scala/Object/HelloWorld.scala
+++ src/main/scala/Object/HelloWorld.scala
@@ -4,5 +4,12 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello World")
   }
-
 }
+
+// Singleton Objects:
+// Scala is more object-oriented than Java because in Scala, we cannot have static members.
+// Instead, Scala has singleton objects.
+// A singleton is a class that can have only one instance, i.e., Object.
+// You create singleton using the keyword object instead of class keyword.
+// Since you can't instantiate a singleton object, you can't pass parameters to the primary constructor.
+// You already have seen all the examples using singleton objects where you called Scala's main method.
